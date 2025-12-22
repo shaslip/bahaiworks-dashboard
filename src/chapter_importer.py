997711afa68c20ae761a9e2680d46b7b8c2480f5
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-12-22 13:27:39 -0800

    Create chapter_importer.py

---
+++ src/chapter_importer.py
@@ -0,0 +1,74 @@
+import json
+from wikibaseintegrator import wbi_helpers
+from wikibaseintegrator.datatypes import String, Item
+from wikibaseintegrator.wbi_enums import ActionIfExists
+from src.wikibase_importer import get_wbi_instance, check_or_create_person
+
+def link_chapter_to_author(wbi, chapter_item_id, author_item_id):
+    author_item = wbi.item.get(entity_id=author_item_id)
+    new_claim = Item(value=chapter_item_id, prop_nr='P11') # has authored
+    author_item.claims.add(new_claim, action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
+    author_item.write()
+
+def link_chapter_to_book(wbi, chapter_item_id, book_item_id):
+    book_item = wbi.item.get(entity_id=book_item_id)
+    new_claim = Item(value=chapter_item_id, prop_nr='P59')  # P59 = has chapter
+    book_item.claims.add(new_claim, action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
+    book_item.write()
+
+def create_chapter_item(wbi, title, page_range, author_item_ids, book_item_id):
+    chapter_item = wbi.item.new()
+    chapter_item.labels.set(language='en', value=title)
+    
+    # P12: Instance of Written Work (Q4581)
+    chapter_item.claims.add(Item(value='Q4581', prop_nr='P12'))
+
+    # P9: Part of Book
+    chapter_item.claims.add(Item(value=book_item_id, prop_nr='P9'))
+
+    # P10: Author(s)
+    for author_id in author_item_ids:
+        chapter_item.claims.add(Item(value=author_id, prop_nr='P10'), 
+                                action_if_exists=ActionIfExists.APPEND_OR_REPLACE)
+
+    # P6: Page Range
+    chapter_item.claims.add(String(value=page_range, prop_nr='P6'))
+    
+    chapter_item.write()
+    return chapter_item.id
+
+def import_chapters_to_wikibase(book_qid, chapters_data):
+    """
+    Orchestrates the creation of chapters and linking them to the parent book.
+    Returns a list of log strings describing what happened.
+    """
+    wbi = get_wbi_instance()
+    logs = []
+
+    for chapter in chapters_data:
+        title = chapter.get('title')
+        page_range = chapter.get('page_range')
+        authors = chapter.get('author', [])
+
+        # 1. Resolve Authors
+        author_ids = []
+        for author_name in authors:
+            # Reusing the robust logic from your other importer
+            aid = check_or_create_person(wbi, author_name, "author")
+            author_ids.append(aid)
+            if aid and not wbi_helpers.search_entities(author_name): 
+                # If it was just created (rough check), log it
+                logs.append(f"Created new author: {author_name} ({aid})")
+
+        # 2. Create Chapter
+        chapter_qid = create_chapter_item(wbi, title, page_range, author_ids, book_qid)
+        logs.append(f"Created chapter: '{title}' ({chapter_qid})")
+
+        # 3. Link Author -> Chapter
+        for aid in author_ids:
+            link_chapter_to_author(wbi, chapter_qid, aid)
+
+        # 4. Link Book -> Chapter
+        link_chapter_to_book(wbi, chapter_qid, book_qid)
+
+    return logs
