d8dda43d534d9be3788bda500cf662778c19514a
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2026-06-05 09:29:38 -0700

    Update face_detection.py

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,3 +1,8 @@
+import os
+# Suppress TensorFlow informational logs and oneDNN warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 import numpy as np
 from mtcnn import MTCNN
 
